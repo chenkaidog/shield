8555e4fec2b72f565cab4a6290b5b0ded520bfd7
chenkaidog <chenkaidong.15@gmail.com>
2023-10-10 01:00:41 +0800

    client test

--- account/test/main.go
+++ account/test/main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"shield/account/kitex_gen/base"
 	"shield/account/kitex_gen/kaidog/shield/account"
 	"shield/account/kitex_gen/kaidog/shield/account/accountservice"
+
+	"github.com/cloudwego/kitex/client"
 )
 
 func main() {
 	ctx := context.Background()
-	client := accountservice.MustNewClient("kaidog.shield.account")
+	client := accountservice.MustNewClient("AccountService", client.WithHostPorts("0.0.0.0:8888"))
 	resp, err := client.Login(ctx, &account.AccountLoginReq{
-		Username: "bob",
-		Password: "123456",
+		Username: "bobbobobo",
+		Password: "12345678",
+		Ipv4: "127.0.0.1",
+		Device: "PC",
+		Base: &base.BaseReq{
+			TraceID: "trace_id",
+			Caller: "client",
+		},
 	})
 	fmt.Println(resp, err)
 }
